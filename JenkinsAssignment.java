f7bf9c18fc017901b67c2ce3acc395d10386357f
irfan339 <37179008+irfan339@users.noreply.github.com>
2019-01-30 21:34:15 +0530

    Delete JenkinsAssignment.java

--- JenkinsAssignment.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-class JenkinsAssignment{
-	public static void main(String args[]){
-		System.out.println("Welcome to SPE");
-		System.out.println("This is assignment.")
-	}
-}
