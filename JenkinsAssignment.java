bb1d862491502326e80b719f9aea96178276d652
irfan339 <kotlasaicharan@yahoo.com>
2019-01-30 21:17:56 +0530

    changed a bit

--- JenkinsAssignment.java
+++ JenkinsAssignment.java
@@ -3,6 +3,6 @@ import java.util.*;
 class JenkinsAssignment{
 	public static void main(String args[]){
 		System.out.println("Welcome to SPE");
-		System.out.println("This is assignment")
+		System.out.println("This is assignment.")
 	}
 }
